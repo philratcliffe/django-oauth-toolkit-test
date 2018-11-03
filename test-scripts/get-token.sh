bad2babc56e38dd467b048eaff2b294887eab08e
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-11-03 13:47:44 +0000

    Add a script to grab a Bearer token from the Auth Server.

---
+++ test-scripts/get-token.sh
@@ -0,0 +1,4 @@
+# Edit this command to meet your requirements.
+# In the example below, a user, test1, has been registered in the auth server. 
+# And the client_id and client_secret have been set to the value test1.
+curl -X POST -d "grant_type=password&username=test1&password=notsecure" -u"test1:test1" http://192.168.1.104:8000/o/token/
