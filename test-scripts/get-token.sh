d66023d50427f25442d96fdae5a4ce9a6806bd6f
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-11-05 13:19:21 +0000

    Added shebang line.

--- test-scripts/get-token.sh
+++ test-scripts/get-token.sh
@@ -1,3 +1,4 @@
+#!/usr/bin/env bash
 # Edit this command to meet your requirements.
 # In the example below, a user, test1, has been registered in the auth server. 
 # And the client_id and client_secret have been set to the value test1.
