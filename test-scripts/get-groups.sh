f4042783806f9c830c9b39c375309e8602406a22
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-11-06 10:11:31 +0000

    Added get-groups.sh script.

---
+++ test-scripts/get-groups.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# Edit this command so it has a valid Bearer token and meets your requirements.
+# Note the token must provide groups scope (required by the GroupList view)
+curl -H "Authorization: Bearer 7v4JhEcxXPMsyZFTF3BDKZagokeSS7" http://192.168.1.104:8000/groups/
