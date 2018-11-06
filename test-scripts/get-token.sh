ea7488ed600b808de0e983aff345bbaa4343c390
Phil Ratcliffe <phil@philratcliffe.co.uk>
2018-11-06 10:12:34 +0000

    Updates to test cURL scripts.

--- test-scripts/get-token.sh
+++ test-scripts/get-token.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Edit this command to meet your requirements.
-# In the example below, a user, test1, has been registered in the auth server. 
+# Edit this command so it has a valid Bearer token and meets your requirements.
+# In the example below, a user, test1, has been registered in the auth server.
 # And the client_id and client_secret have been set to the value test1.
 curl -X POST -d "grant_type=password&username=test1&password=notsecure&scope=read" -u"test1:test1" http://192.168.1.104:8000/o/token/
--- test-scripts/get-users.sh
+++ test-scripts/get-users.sh
@@ -1,2 +1,3 @@
-# Edit this command to meet your requirements.
-curl -H "Authorization: Bearer olndvws5NDtZehNKbfdLyJgNHRJGlS" http://192.168.1.104:8000/users/ | json_pp
+#!/usr/bin/env bash
+# Edit this command so it has a valid Bearer token and meets your requirements.
+curl -H "Authorization: Bearer 7v4JhEcxXPMsyZFTF3BDKZagokeSS7" http://192.168.1.104:8000/users/ | json_pp
